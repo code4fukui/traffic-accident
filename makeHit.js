70b964aec57523285a38fe666ee9e96c28c4b584
Taisuke Fukuno <fukuno@jig.jp>
2022-07-09 10:10:33 +0900

    add 衝突部位

---
+++ makeHit.js
@@ -0,0 +1,25 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+
+const names = [
+  "無",
+  "前",
+  "右",
+  "後",
+  "左",
+  "右前",
+  "右後",
+  "左後",
+  "左前",
+];
+
+const list = [];
+for (let i = 0; i < names.length; i++) {
+  for (let j = 0; j < names.length; j++) {
+    list.push({
+      code: i + "" + j,
+      value: names[i] + "-" + names[j],
+      description: "",
+    });
+  }
+}
+await Deno.writeTextFile("code/hit.csv", CSV.stringify(list));

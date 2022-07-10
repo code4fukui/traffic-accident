c4e621c5399b908b4c6a9d720854c5fc1890575f
Taisuke Fukuno <fukuno@jig.jp>
2022-07-10 17:55:46 +0900

    update data

--- make.js
+++ make.js
@@ -1,5 +1,6 @@
 import {} from "./makeFukui.js";
 import {} from "./makeEhime.js";
+import {} from "./makeOsaka.js";
 import {} from "./makeFatal.js";
 import {} from "./makeKosoku.js";
-import {} from "./makeKosokuStat.js";
+//import {} from "./makeKosokuStat.js";
